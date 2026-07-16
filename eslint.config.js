80f9d9a75f0ae98bc7736537c8a2ce7aa5c4a2a8
Chloe <caglassonbury@gmail.com>
2026-07-16 12:09:05 +0100

    chore: add storybook to project

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -6,23 +9,20 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import reactX from "eslint-plugin-react-x";
 import reactDom from "eslint-plugin-react-dom";
 
-export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.strictTypeChecked,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-      reactX.configs["recommended-typescript"],
-      reactDom.configs.recommended,
-    ],
-    languageOptions: {
-      parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-        tsconfigRootDir: import.meta.dirname,
-      },
+export default defineConfig([globalIgnores(["dist"]), {
+  files: ["**/*.{ts,tsx}"],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    reactHooks.configs.flat.recommended,
+    reactRefresh.configs.vite,
+    reactX.configs["recommended-typescript"],
+    reactDom.configs.recommended,
+  ],
+  languageOptions: {
+    parserOptions: {
+      project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-]);
+}, ...storybook.configs["flat/recommended"]]);
