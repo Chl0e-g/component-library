9f13e9edb3a188f19ce3d7928fd98e2fc9fb3f1b
Chloe <caglassonbury@gmail.com>
2026-07-19 10:40:01 +0100

    chore: add Flex component export to index

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export type { TTextProps } from "./components/foundations/text/Text.tsx";
 
 export { Box } from "./components/foundations/box/Box.tsx";
 export type { TBoxProps } from "./components/foundations/box/Box.tsx";
+
+export { Flex } from "./components/foundations/flex/Flex.tsx";
+export type { TFlexProps } from "./components/foundations/flex/Flex.tsx";
